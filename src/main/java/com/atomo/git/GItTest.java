b43d5f28f5b394b7c2102c0370d5e0a425d22cfe
kallen <kallen@qq.com>
2022-02-18 18:33:42 +0800

    hot-fix commit1

--- src/main/java/com/atomo/git/GItTest.java
+++ src/main/java/com/atomo/git/GItTest.java
@@ -9,5 +9,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("hello,hot-fix!");
     }
 }
