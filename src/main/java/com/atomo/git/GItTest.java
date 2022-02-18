63e1cfe5bed845651b3b0ee7288e38b98816ca47
kallen <kallen@qq.com>
2022-02-18 15:14:06 +0800

    first commit

---
+++ src/main/java/com/atomo/git/GItTest.java
@@ -0,0 +1,12 @@
+package com.atomo.git;
+
+/**
+ * @name:GItTest
+ * @author:kallen
+ * @time:2022/2/18 13:28
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
